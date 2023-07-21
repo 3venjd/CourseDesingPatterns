baa25255c3baf46e32cee07ee813238c8cb17b3a
luisj@sciww.com <luisj@sciww.com>
2023-07-21 08:58:45 -0500

    First Video end

---
+++ IntroductionDesignPatterns/Program.cs
@@ -0,0 +1,27 @@
+﻿
+
+
+Person person1 = new Person("Evelio",31,"Colombian");
+Person person2 = new Person("Cristina", 37, "Colombian");
+Console.WriteLine(person1.Show());
+Console.WriteLine(person2.Show());
+
+
+class Person
+{
+    public string name;
+    public int age;
+    public string nationality;
+
+    public Person(string _name, int _age, string _nationality)
+    {
+        name = _name;
+        age = _age;
+        nationality = _nationality;
+    }
+
+    public string Show()
+    {
+        return name + " " + age + " " + nationality;
+    }
+}
